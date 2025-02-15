c9ded4da5b8f7d64b7198f2c7a7c2094b9ab16d0
vishalkrsharma <vishalkrsharma1234@gmail.com>
2025-02-15 20:40:15 +0530

    fix: build errors

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,8 @@
 import { env } from '@/lib/env';
 import axios from 'axios';
 
+export const runtime = 'nodejs';
+
 export const api = axios.create({
   baseURL: env.SERVER_URL + '/api',
   withCredentials: true,
